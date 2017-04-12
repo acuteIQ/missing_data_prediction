f6588ea97120ef6940f48e0325e19ff28c0dec76
Can Kavaklıoğlu <eposta@cankavaklioglu.name.tr>
2017-04-12 10:22:25 +0300

    added null and 0 checks for line counting and unique value detection, added loop for all file types

--- dataset_analysis.py
+++ dataset_analysis.py
@@ -2,49 +2,73 @@ import os
 import csv
 import json
 DATA_DIR='../data/data_export_ck'
+ALL_DATA_FILE_PREFIX=['tbl_company', 'tbl_equifax',  'tbl_exp_matches']
 
-all_column_names=[]
-all_column_counts=[]
-data_line_count=0
-extract_unique_columns=['Company_Name', 'city', 'zip', 'industry_sic_code']
+all_column_names={}
+all_column_counts={}
+data_line_count={}
+extract_unique_columns= {
+    'tbl_company':['Company_Name', 'city', 'zip', 'industry_sic_code'],
+    'tbl_equifax':['CLIENT_NAME', 'CLIENT_CITY', 'CLIENT_ZIP', 'EFX_PRIMSIC'],
+    'tbl_exp_matches':[]
+}
 extract_unique_columns_data={}
 
-for filename in os.listdir(DATA_DIR):
-    print filename
-    if filename.startswith('tbl_company'):
-        f=open(os.path.join(DATA_DIR,filename))
-        csvreader=csv.reader(f)
-        first_line=True
-        for line in csvreader:
-            if first_line: # skip first line of each file
-                first_line=False
-                if not all_column_names:
-                    all_column_names=line
-                    all_column_counts = [0]*len(line)
-            else:
-                data_line_count+=1
-                for column_index, column_data in enumerate(line):
-                    if column_data:
-                        all_column_counts[column_index]+=1
-                        column_name=all_column_names[column_index]
-                        if column_name in extract_unique_columns:
-                            if column_name not in extract_unique_columns_data:
-                                extract_unique_columns_data[column_name] = set()
-                            extract_unique_columns_data[column_name].add(column_data)
-        f.close()
+for DATA_FILE_PREFIX in ALL_DATA_FILE_PREFIX:
+    all_column_counts[DATA_FILE_PREFIX]=0
+    data_line_count[DATA_FILE_PREFIX]=0
+    print 'DATA_FILE_PREFIX:', DATA_FILE_PREFIX
+    for filename in os.listdir(DATA_DIR):
+        if filename.startswith(DATA_FILE_PREFIX):
+            print filename
+            f=open(os.path.join(DATA_DIR,filename))
+            csvreader=csv.reader(f)
+            first_line=True
+            for line in csvreader:
+                if first_line: # skip first line of each file
+                    first_line=False
+                    if DATA_FILE_PREFIX not in all_column_names:
+                        all_column_names[DATA_FILE_PREFIX]=line
+                        all_column_counts[DATA_FILE_PREFIX] = [0]*len(line)
+                        extract_unique_columns_data[DATA_FILE_PREFIX]={}
+                else:
+                    data_line_count[DATA_FILE_PREFIX]+=1
+                    for column_index, column_data in enumerate(line):
+                        if column_data:
 
-#print 'data_line_count', data_line_count
-#print all_column_counts
-f=open('company_all_column_counts.json', 'w')
+                            valid_data=True
+                            try:
+                                column_data_int=int(column_data)
+                                if column_data_int == 0:
+                                    valid_data=False
+                            except:
+                                pass
+
+                            if column_data == 'null':
+                                valid_data=False
+
+                            if valid_data:
+                                all_column_counts[DATA_FILE_PREFIX][column_index]+=1
+                                column_name=all_column_names[DATA_FILE_PREFIX][column_index]
+                                if column_name in extract_unique_columns[DATA_FILE_PREFIX]:
+                                    if column_name not in extract_unique_columns_data[DATA_FILE_PREFIX]:
+                                        extract_unique_columns_data[DATA_FILE_PREFIX][column_name] = set()
+                                    extract_unique_columns_data[DATA_FILE_PREFIX][column_name].add(column_data)
+            f.close()
+
+f=open('data_analysis.json', 'w')
 # set is not json exportable
-for column_name in extract_unique_columns_data:
-    extract_unique_columns_data[column_name]=list(extract_unique_columns_data[column_name])
+for DATA_FILE_PREFIX in ALL_DATA_FILE_PREFIX:
+    for column_name in extract_unique_columns_data[DATA_FILE_PREFIX]:
+        extract_unique_columns_data[DATA_FILE_PREFIX][column_name]=list(extract_unique_columns_data[DATA_FILE_PREFIX][column_name])
+
 output={
+    'all_data_file_prefix':ALL_DATA_FILE_PREFIX,
     'all_column_names':all_column_names,
     'all_column_counts':all_column_counts,
     'data_line_count':data_line_count,
     'extract_unique_columns':extract_unique_columns,
     'extract_unique_columns_data':extract_unique_columns_data
-    }
+}
 f.write(json.dumps(output))
 f.close()
--- dataset_analysis_printer.py
+++ dataset_analysis_printer.py
@@ -1,17 +1,19 @@
 import json
-f=open('company_all_column_counts.json')
+f=open('data_analysis.json')
 input_data=json.loads(f.read())
 f.close()
 
-print 'total number of rows', input_data['data_line_count']
-result=[]
-for company_name_index, company_name in enumerate(input_data['all_column_names']):
-    result.append( (company_name, input_data['all_column_counts'][company_name_index]) )
+for DATA_FILE_PREFIX in input_data['all_data_file_prefix']:
+    print "DATA_FILE_PREFIX:", DATA_FILE_PREFIX
+    print 'total number of rows', input_data['data_line_count'][DATA_FILE_PREFIX]
+    result=[]
+    for company_name_index, company_name in enumerate(input_data['all_column_names'][DATA_FILE_PREFIX]):
+        result.append( (company_name, input_data['all_column_counts'][DATA_FILE_PREFIX][company_name_index]) )
 
-print 'column_name', 'number of occurrence', 'occurrence percentage'
-for r in sorted(result, key=lambda x:x[1], reverse=True):
-    print r[0], r[1], '%.0f'%(r[1]/float(input_data['data_line_count'])*100)
+    print 'column_name', 'number of occurrence', 'occurrence percentage'
+    for r in sorted(result, key=lambda x:x[1], reverse=True):
+        print r[0], r[1], '%.0f'%(r[1]/float(input_data['data_line_count'][DATA_FILE_PREFIX])*100)
 
-print 'column_name', 'number of unique data'
-for extract_unique_column in input_data['extract_unique_columns']:
-    print extract_unique_column, len(input_data['extract_unique_columns_data'][extract_unique_column])
+    print 'column_name', 'number of unique data'
+    for extract_unique_column in input_data['extract_unique_columns'][DATA_FILE_PREFIX]:
+        print extract_unique_column, len(input_data['extract_unique_columns_data'][DATA_FILE_PREFIX][extract_unique_column])
