3419900e42e51d26893dcd1de90c451d6efbda7e
Can Kavaklıoğlu <eposta@cankavaklioglu.name.tr>
2017-08-09 20:35:45 -0400

    repository restructure before tf work

---
+++ vacuum.sql
@@ -0,0 +1,12 @@
+\echo 1
+vacuum full company;
+\echo 2
+vacuum full company1;
+\echo 3
+vacuum full company2;
+\echo 4
+vacuum full company3;
+\echo 5
+vacuum full company_prediction;
+\echo 6
+vacuum full equifax;
